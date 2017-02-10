b5dd652e64ff601437c8cde1b9a36a73658a2742
mapqlz <dpate226@uwo.ca>
2017-02-09 22:40:01 -0500

    Added fulltime instance variable to Student.

--- src/Student.java
+++ src/Student.java
@@ -3,10 +3,11 @@
  */
 public class Student extends Person {
     String major;
-
-    public Student(String name, String emailAddress, String major) {
+    boolean fulltime;
+    public Student(String name, String emailAddress, String major,boolean fulltime) {
         super(name, emailAddress);
         this.major = major;
+        this.fulltime = fulltime;
     }
 
     public String toString() {
